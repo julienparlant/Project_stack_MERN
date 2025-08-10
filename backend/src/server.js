32d5b002f6462ef0ca607e194b65b8cb6400fbd8
Julien Parlant <julien.parlant@gmail.com>
2025-08-10 13:33:06 +0200

    Connecting the DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config(); 
 
+ const app = express();
+ const PORT = process.env.PORT || 3000;
+ 
 app.use('/api/notes', notesRoutes);
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+connectDB();
+
+app.use(express.json()); 
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT:", PORT);
 });
+
