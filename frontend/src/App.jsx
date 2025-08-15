ae186a39a94ba4d53666b10c2ee7f0507ccbad08
Julien Parlant <julien.parlant@gmail.com>
2025-08-15 17:08:16 +0200

    Adding DaisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,20 @@
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from "react-router";
 
-import HomePage from './pages/HomePage'
-import CreatePage from './pages/CreatePage'
-import NoteDetailPage from './pages/NoteDetailPage'
-import toast from 'react-hot-toast'
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>
-      <button onClick={() => toast.success("Congrats")} className="bg-blue-500 text-white px-4 py-2 rounded">Click me</button>
+    <div data-theme="synthwave" className="min-h-screen bg-base-200">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [daisyui],
+  daisyui: {
+  themes: ["pastel", "cupcake", "coffee", "dark", "light", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "dracula"],
+  }
+};
