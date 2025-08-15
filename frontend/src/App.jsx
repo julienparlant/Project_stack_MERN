08b954e52cdd9635fb5f24e02d431f05605bea36
Julien Parlant <julien.parlant@gmail.com>
2025-08-15 13:55:58 +0200

    Toast Configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
 import { Routes, Route } from 'react-router'
+
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage'
+import toast from 'react-hot-toast'
 
 const App = () => {
   return (
     <div>
+      <button onClick={() => toast.success("Congrats")}>Click me</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
